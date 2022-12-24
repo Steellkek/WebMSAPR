b4571a26a13500ce357ae900b32abd854f29618f
Steellkek <alya.min80@gmail.com>
2022-12-24 19:35:31 +0300

    ю

--- WebMSAPR/wwwroot/js/graph.js
+++ WebMSAPR/wwwroot/js/graph.js
@@ -1,6 +1,6 @@
 // photos from flickr with creative commons license
 
-var cy = cytoscape({
+/*var cy = cytoscape({
     container: document.getElementById('cy'),
 
     boxSelectionEnabled: false,
@@ -103,13 +103,12 @@ var cy = cytoscape({
 
         allowNodesInsideCircle: true,
 
-    }*/
-}); // cy init
-async function  graph() {
-    let cy = cytoscape({
+    }
+});*/ // cy init
 
-        container: document.getElementById('cy'),
-    })
+var cy = window.cy = cytoscape({});
+async function  graph() {
+    
     
     var layout = cy.layout({
         name: 'cise',
@@ -177,8 +176,7 @@ async function  gengraph(){
     if (response.ok) {
         const graph = await response.json();
         console.log(graph);
-        let cy = cytoscape({
-
+        var cy = window.cy = cytoscape({
             container: document.getElementById('cy'),
 
             elements: [],
@@ -225,7 +223,7 @@ async function  gengraph(){
                         id: 'e' + (x+1),
                         source: 'g' + graph.modules[i].connectionsInModules[j]._element1.number,
                         target: 'g' + graph.modules[i].connectionsInModules[j]._element2.number,
-                        label: graph.connectionsBeetwenModules[i].value
+                        label: graph.modules[i].connectionsInModules[j]._value
                     }
                 }])
                 x+=1;
@@ -242,8 +240,7 @@ async function  gengraph(){
             }])
             x+=1;
         }
-        debugger;
-        var layout = cy.layout({
+         cy.layout= cy.layout({
             name: 'fcose',
             quality: "default",
             randomize: true,
@@ -256,16 +253,20 @@ async function  gengraph(){
             gravityRangeCompound: 1.5,
             gravityCompound: 1.0
         });
-        layout.run();
         //layout.stop();
         
+        
     }
+    cy.layout.run();
+    cy.layout.stop();
+
+    console.log(cy._private.elements)
 }
 
 function newgraph(){
     
 
-        var cy = window.cy = cytoscape({
+        cy = cytoscape({
             container: document.getElementById('cy'),
 
             layout: {
@@ -274,183 +275,79 @@ function newgraph(){
 
             style: [
                 {
-                    selector: 'node',
+                    selector: 'node[name]',
                     style: {
-                        'label': 'data(id)'
+                        'content': 'data(name)'
                     }
                 },
 
                 {
-                    selector: '.mid',
+                    selector: 'edge',
                     style: {
-                        'width': 8,
-                        'height': 8,
-                        'label': ''
+                        'curve-style': 'bezier',
+                        'target-arrow-shape': 'triangle'
+                    }
+                },
+
+                // some style for the extension
+
+                {
+                    selector: '.eh-handle',
+                    style: {
+                        'background-color': 'red',
+                        'width': 12,
+                        'height': 12,
+                        'shape': 'ellipse',
+                        'overlay-opacity': 0,
+                        'border-width': 12, // makes the handle easier to hit
+                        'border-opacity': 0
+                    }
+                },
+
+                {
+                    selector: '.eh-hover',
+                    style: {
+                        'background-color': 'red'
+                    }
+                },
+
+                {
+                    selector: '.eh-source',
+                    style: {
+                        'border-width': 2,
+                        'border-color': 'red'
+                    }
+                },
+
+                {
+                    selector: '.eh-target',
+                    style: {
+                        'border-width': 2,
+                        'border-color': 'red'
+                    }
+                },
+
+                {
+                    selector: '.eh-preview, .eh-ghost-edge',
+                    style: {
+                        'background-color': 'red',
+                        'line-color': 'red',
+                        'target-arrow-color': 'red',
+                        'source-arrow-color': 'red'
+                    }
+                },
+
+                {
+                    selector: '.eh-ghost-edge.eh-preview-active',
+                    style: {
+                        'opacity': 0
                     }
                 }
             ],
 
-            elements: [
-                { data: { id: 'a' } },
-                { data: { id: 'b' } },
-                { data: { id: 'c' } },
-                { data: { id: 'mid' }, classes: 'mid' },
-                { data: { source: 'a', target: 'mid' } },
-                { data: { source: 'b', target: 'mid' } },
-                { data: { source: 'mid', target: 'c' } },
-
-                { data: { id: 'd' } },
-
-                { data: { id: 'e' } },
-                { data: { id: 'f' } },
-                { data: { source: 'e', target: 'f' } },
-
-                { data: { id: 'g' } },
-
-                { data: { id: 'h' } },
-                { data: { id: 'i' } },
-                { data: { source: 'h', target: 'i' } },
-
-                { data: { id: 'j' } },
-
-                { data: { id: 'k' } },
-                { data: { id: 'm' } },
-                { data: { id: 'n' } },
-                { data: { source: 'k', target: 'm' } },
-                { data: { source: 'k', target: 'n' } },
-                { data: { source: 'm', target: 'n' } },
-            ]
+            
         });
-
-        // cy.on('automove', function( evt ){
-        // 	var target = evt.target || evt.cyTarget; // 3.x || 2.x
-        //
-        // 	console.log('automove event on %s', target.id());
-        // });
-
-        // a, b, c; with mid in the middle
-
-        cy.$('#a, #b, #c').makeLayout({
-            name: 'circle',
-            boundingBox: {
-                x1: 0,
-                y1: 0,
-                x2: 300,
-                y2: 300
-            }
-        }).run();
-
-        cy.automove({
-            nodesMatching: cy.$('#mid'),
-            reposition: 'mean',
-            meanOnSelfPosition: function( node ){ return false; }
-        });
-
-        // dragging mid drags its neighbourhood with it
-        cy.automove({
-            nodesMatching: cy.$('#mid').neighbourhood().nodes(),
-            reposition: 'drag',
-            dragWith: cy.$('#mid')
-        });
-
-
-        // d can't go out of a box
-
-        cy.automove({
-            nodesMatching: cy.$('#d'),
-            reposition: { x1: 350, x2: 450, y1: 100, y2: 200 }
-        });
-
-        cy.$('#d').position({ x: 400, y: 150 });
-
-
-        // e & f have the same y
-
-        var eAndF = cy.$('#e, #f');
-
-        eAndF.makeLayout({
-            name: 'grid',
-            boundingBox: { x1: 0, x2: 300, y1: 300, y2: 400 },
-            cols: 2,
-            fit: false
-        }).run();
-
-        cy.automove({
-            nodesMatching: cy.$('#e, #f'),
-            reposition: function( node ){
-                var pos = node.position();
-
-                if( node.grabbed() ){ return pos; }
-
-                var otherNode = eAndF.not( node );
-
-                return {
-                    x: pos.x,
-                    y: otherNode.position('y')
-                };
-            },
-            when: 'matching'
-        });
-
-        // g kept in viewport
-
-        cy.$('#g').position({ x: 400, y: 350 });
-
-        cy.fit( 100 ); // make sure g is in the viewport for the demo
-
-        cy.automove({
-            nodesMatching: cy.$('#g'),
-            reposition: 'viewport'
-        });
-
-
-        // i gets pulled along with h on drag
-
-        cy.$('#h').position({ x: 585, y: 195 });
-        cy.$('#i').position({ x: 510, y: 260 });
-
-        cy.automove({
-            nodesMatching: cy.$('#i'),
-            reposition: 'drag',
-            dragWith: cy.$('#h')
-        });
-
-
-        // j can't go in the box of d
-
-        cy.$('#j').position({ x: 585, y: 350 });
-
-        cy.automove({
-            nodesMatching: cy.$('#j'),
-            reposition: { type: 'outside', x1: 350, x2: 450, y1: 100, y2: 200 }
-        });
-
-
-        // k, m, n all move together on drag as a unit (e.g. cluster)
-
-        cy.$('#k').position({ x: 430, y: -20 });
-        cy.$('#m').position({ x: 490, y: -110 });
-        cy.$('#n').position({ x: 550, y: -20 });
-
-        cy.automove({
-            nodesMatching: cy.$('#k, #m, #n'),
-            reposition: 'drag',
-            dragWith: cy.$('#k, #m, #n')
-        });
-
-
-
-        cy.fit( 100 ); // fit to all the layouts
-
-
-        // .automove-viewport nodes kept in viewport (even if added after this call)
-        // convenient but less performant than `nodesMatching: collection`
-
-        cy.automove({
-            nodesMatching: '.automove-viewport',
-            reposition: 'viewport'
-        });
-
+        
         cy.on('tap', function( evt ){
             var tgt = evt.target || evt.cyTarget; // 3.x || 2.x
 
@@ -471,9 +368,133 @@ function newgraph(){
 
             tgt.remove();
         })
+
+    var eh = cy.edgehandles({
+        snap: false
+    });
+
+    eh.enableDrawMode();
+
+
     console.log(cy)
         
 }
 document.getElementById("x").addEventListener("click", function() {
     console.log(cy)
 })
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    var cy = window.cy = cytoscape({
+        container: document.getElementById('cy'),
+
+        layout: {
+            name: 'concentric',
+            concentric: function(n){ return n.id() === 'j' ? 200 : 0; },
+            levelWidth: function(nodes){ return 100; },
+            minNodeSpacing: 100
+        },
+
+        style: [
+            {
+                selector: 'node[name]',
+                style: {
+                    'content': 'data(name)'
+                }
+            },
+
+            {
+                selector: 'edge',
+                style: {
+                    'curve-style': 'bezier',
+                    'target-arrow-shape': 'triangle'
+                }
+            },
+
+            // some style for the extension
+
+            {
+                selector: '.eh-handle',
+                style: {
+                    'background-color': 'red',
+                    'width': 12,
+                    'height': 12,
+                    'shape': 'ellipse',
+                    'overlay-opacity': 0,
+                    'border-width': 12, // makes the handle easier to hit
+                    'border-opacity': 0
+                }
+            },
+
+            {
+                selector: '.eh-hover',
+                style: {
+                    'background-color': 'red'
+                }
+            },
+
+            {
+                selector: '.eh-source',
+                style: {
+                    'border-width': 2,
+                    'border-color': 'red'
+                }
+            },
+
+            {
+                selector: '.eh-target',
+                style: {
+                    'border-width': 2,
+                    'border-color': 'red'
+                }
+            },
+
+            {
+                selector: '.eh-preview, .eh-ghost-edge',
+                style: {
+                    'background-color': 'red',
+                    'line-color': 'red',
+                    'target-arrow-color': 'red',
+                    'source-arrow-color': 'red'
+                }
+            },
+
+            {
+                selector: '.eh-ghost-edge.eh-preview-active',
+                style: {
+                    'opacity': 0
+                }
+            }
+        ],
+
+        elements: {
+            nodes: [
+                { data: { id: 'j', name: 'Jerry' } },
+                { data: { id: 'e', name: 'Elaine' } },
+                { data: { id: 'k', name: 'Kramer' } },
+                { data: { id: 'g', name: 'George' } }
+            ],
+            edges: [
+                { data: { source: 'j', target: 'e' } },
+                { data: { source: 'j', target: 'k' } },
+                { data: { source: 'j', target: 'g' } },
+                { data: { source: 'e', target: 'j' } },
+                { data: { source: 'e', target: 'k' } },
+                { data: { source: 'k', target: 'j' } },
+                { data: { source: 'k', target: 'e' } },
+                { data: { source: 'k', target: 'g' } },
+                { data: { source: 'g', target: 'j' } }
+            ]
+        }
+    });
+
+    var eh = cy.edgehandles({
+        snap: false
+    });
+    
+
+    document.querySelector('#start').addEventListener('click', function() {
+        eh.start( cy.$('node:selected') );
+    });
+
+});
